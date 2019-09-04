94141bd8d03277be52426b687d768facfdf24937
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2019-09-04 05:45:23 +0200

    Documentation and cleaning of command_pilot done.

--- commands_pilot/src/Pilot.h
+++ commands_pilot/src/Pilot.h
@@ -12,33 +12,69 @@
 
 class Pilot {
 public:
-    enum class Commands {FORWARD, LEFT, RIGHT};
+
+    //Enumerates and structs
+    /// Set of directions the robot can take.
     enum class Dir{UP, DOWN, LEFT, RIGHT};
 
-    Pilot(int argc, char **argv);
-
-    std::queue<std::string> commands;
-
+    //Publishers and subscribers
+    /// Publisher for movement of the robot,
     ros::Publisher cmdVelPublisher;
+    /// Subscriber for odometry.
     ros::Subscriber odomSubscriber;
+
+    //Callback functions
+    /// Callback function for odometry.
+    /// \param msg Message from odometer.
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
-    int getYaw(const nav_msgs::Odometry::ConstPtr& msg);
-    void updateHeading(bool left);
 
+    //Robot instructions.
+    /// Go forward.
     void goForward();
+    /// Turn left.
     void turnLeft();
+    /// Turn right.
     void turnRight();
+    /// Stop robot.
     void stop();
 
-    void readCommands();
+    //Class functions.
+    /// Change heading of the robot.
+    /// \param left If left, true. False otherwise.
+    void updateHeading(bool left);
 
+    //Helpers
+    /// Get yaw angle of the robot in degrees.
+    /// \param msg Message from odometry.
+    /// \return Yaw angle in degrees.
+    int getYaw(const nav_msgs::Odometry::ConstPtr& msg);
+
+    //Flags
+    /// Flag for store memory.
+    int flag_init;
+
+    //Memory of movement.
+    /// Memory for last command executed.
+    int currentCommand;
+    /// Memory from last position and angle before start moving.
     float posX, posY;
     float turnZ;
-    int currentCommand;
 
-    int flag_init;
+    //Robot orientation and position.
+    /// Direction where the robot is heading.
     Dir heading;
+
+    //IO functions
+    /// Read commands from a file.
+    void readCommands();
+
+    //Constructor and destructor
+    /// Constructor of pilot.
+    Pilot(int argc, char **argv);
+    
+    std::queue<std::string> commands;
+
 };
 
 
