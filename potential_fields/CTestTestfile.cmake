3bf8923c565dff2e998ea54b1b571768d7baa662
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2019-09-04 03:55:15 +0200

    Cleaned rare files.

--- potential_fields/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alberto/catkin_ws/src/potential_fields
-# Build directory: /home/alberto/catkin_ws/src/potential_fields
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
